eac2e4f5f200430d29dc4af3c1fbeebf4d7fa039
Gustavo Pagani <gustavo.s.pagani@gmail.com>
2025-11-12 08:10:52 -0300

    .

--- puzzle.c
+++ puzzle.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <assert.h>
 #include "puzzle.h"
 
 void gerar_tabuleiro_aleatorio(int vetor[9]) {
-    srand(time(NULL));
 
     for (int i = 0; i < 9; i++)
         vetor[i] = i;
@@ -17,7 +15,6 @@ void gerar_tabuleiro_aleatorio(int vetor[9]) {
         vetor[j] = temp;
     }
 
-    assert(vetor != NULL);
 }
 
 void preencher_tabuleiro(int vetor[9], int matriz[3][3]) {
@@ -31,7 +28,7 @@ void imprimir_tabuleiro(int matriz[3][3]) {
     printf("\n+---+---+---+\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            matriz[i][j] == 0    ? printf("| X ")  : printf("| %d ", matriz[i][j]);
+            matriz[i][j] == 0    ? printf("| Y ")  : printf("| %d ", matriz[i][j]);
         }
         printf("|\n+---+---+---+\n");
     }
